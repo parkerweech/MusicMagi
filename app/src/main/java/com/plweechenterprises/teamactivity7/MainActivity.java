288e7ddf05f01ae4427a9696fae7f06c365090e4
jcurtis369 <jcurtis369@users.noreply.github.com>
2017-02-15 14:31:47 -0700

    Testing merge conflicts

--- app/src/main/java/com/plweechenterprises/teamactivity7/MainActivity.java
+++ app/src/main/java/com/plweechenterprises/teamactivity7/MainActivity.java
@@ -14,4 +14,5 @@ public class MainActivity extends AppCompatActivity {
     String happyMessage = "HELLO WORLD!!!!!!!";
     // Nice message! :)
     // Testing Push and Pull
+    // Testing merge conflicts
 }
