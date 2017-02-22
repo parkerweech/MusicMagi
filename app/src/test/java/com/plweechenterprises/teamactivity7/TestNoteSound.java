5ed84f46d001ad9ee50e2be3bf7e1cca11f52151
Parker Weech <parkerweech.wei@gmail.com>
2017-02-22 13:45:08 -0700

    Created TestNoteSound

---
+++ app/src/test/java/com/plweechenterprises/teamactivity7/TestNoteSound.java
@@ -0,0 +1,36 @@
+package com.plweechenterprises.teamactivity7;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by admin on 2/22/2017.
+ */
+
+public class TestNoteSound {
+    @Test
+    public void sound_isCorrect() {
+/*
+        byte[] buffer = new byte[32];
+        int numBytes = 0;
+        int channel = 3; // MIDI channels 1-16 are encoded as 0-15.
+        buffer[numBytes++] = (byte)(0x90 + (channel - 1)); // note on
+        buffer[numBytes++] = (byte)60; // pitch is middle C
+        buffer[numBytes++] = (byte)127; // max velocity
+        int offset = 0;
+
+
+        Note temp1 = new Note;
+        Note temp2 = new Note;
+        List tempNoteList = new List<Note>();
+        tempNoteList.add(temp1);
+        tempNoteList.add(temp2);
+
+        assertEquals(tempNoteList.get(1).getSound().getbuffer()[1] == (byte)60);
+        assertEquals(tempNoteList.get(2).getSound().getbuffer()[1] == (byte)85);
+        */
+    }
+}
