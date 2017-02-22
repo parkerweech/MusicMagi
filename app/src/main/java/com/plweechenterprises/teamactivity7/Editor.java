76d2d517963cd76dba3b32a5f9dfea3090983fa2
ambersto <sto12021@byui.edu>
2017-02-22 14:44:43 -0700

    Added editor activity

---
+++ app/src/main/java/com/plweechenterprises/teamactivity7/Editor.java
@@ -0,0 +1,48 @@
+package com.plweechenterprises.teamactivity7;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Editor extends AppCompatActivity {
+
+    List<Note> noteList = new List<Note>;
+    Note note;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+    }
+
+    public void selectNote() {
+        //dropdown list with notes and rests
+    }
+
+    public void selectLength() {
+        //dropdown list with length of note
+    }
+
+    public void addNote() {
+        //adds note to noteList
+    }
+
+    public void display() {
+        //update display with x number of notes
+    }
+
+    public void fullScreen() {
+        //change view to FullScreen activity
+    }
+
+    public void delete() {
+        //delete the last note on noteList
+    }
+
+    public void save() {
+        //change view to Save activity
+    }
+
+    public void play() {
+        //change view to FullScreen activity and play the music
+    }
+}
