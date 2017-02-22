6b0b53571af9f9a9b57c9d31473f2f9ff01f22a5
ambersto <sto12021@byui.edu>
2017-02-22 13:35:47 -0700

    Adding test class for the Save activity

---
+++ app/src/test/java/com/plweechenterprises/teamactivity7/SaveFileTest.java
@@ -0,0 +1,16 @@
+package com.plweechenterprises.teamactivity7;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Amber on 2/22/2017.
+ */
+
+public class SaveFileTest {
+    @Test
+    public void fileSavesCorrectly() throws Exception {
+        assertTrue(Save.saveFile());
+    }
+}
