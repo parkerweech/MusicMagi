4aa183e95f11950e7c833178127c6c7e2a45b8e9
jcurtis369 <jcurtis369@users.noreply.github.com>
2017-03-10 13:36:37 -0700

    Fixed full screen crashing.

--- app/src/main/java/com/plweechenterprises/teamactivity7/FullScreenActivity.java
+++ app/src/main/java/com/plweechenterprises/teamactivity7/FullScreenActivity.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 public class FullScreenActivity extends AppCompatActivity {
 
-
+    // private List<Note> noteList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,18 +22,46 @@ public class FullScreenActivity extends AppCompatActivity {
         setContentView(R.layout.activity_full_screen);
         getSupportActionBar().setTitle("Music Magi");
 
+        /*
+        Log.d("List of notes", "Deserializing the note list");
+        // Toast.makeText(this, "Deserializing the note list", Toast.LENGTH_SHORT).show();
 
+        Intent intent = getIntent();
+        String json = intent.getStringExtra("notes");
+        if(json.length() > 0) {
+
+            Toast.makeText(this,"Hello World",Toast.LENGTH_SHORT).show();
+            Gson gson = new Gson();
+
+            NoteListContainer noteListContainer = gson.fromJson(json, NoteListContainer.class);
+            for(int i = 0; i < noteListContainer.getNoteList().size(); i++) {
+                noteList.add(noteListContainer.getNoteList().get(i));
+            }
+        }
+        */
     }
 
     // display list of music
     // currently the display is a scrollable view and may need to be changed
     public void displayFull() {
+        /*
+        for (int i = 0; i <= noteList.size(); i++) {
+            // Display note
 
+            i++;
+        }
+        */
     }
 
     // start playing the all of the music in the current file
     public void playFull() {
+        /*
+        for (int i = 0; i <= noteList.size(); i++) {
+            // play note
 
+            i++;
+        }
+        */
     }
 
     // change view to Save File screen
