f58e8aa4557eedc84039926b0fac0ecab30161c5
jcurtis369 <jcurtis369@users.noreply.github.com>
2017-03-15 14:58:21 -0600

    Added javadoc notes to the FullScreenActivity.java

--- app/src/main/java/com/plweechenterprises/teamactivity7/MusicEditor.java
+++ app/src/main/java/com/plweechenterprises/teamactivity7/MusicEditor.java
@@ -166,8 +166,6 @@ public class MusicEditor extends AppCompatActivity implements AdapterView.OnItem
         startActivity(intent);
     }
 
-
-
     //Playing the music!!!
 
     public void playNotes(View view) throws InterruptedException {
